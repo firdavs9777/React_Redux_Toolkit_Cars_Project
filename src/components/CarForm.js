c3c76e0090ade8cfcca57fa3643fe45786334887
firdavs9777 <fmutalipov97@gmail.com>
2024-11-25 16:35:57 +0900

    first commit

---
+++ src/components/CarForm.js
@@ -0,0 +1,8 @@
+function CarForm() {
+  return (
+    <div>
+      <h1> Car Form is here working fine </h1>
+    </div>
+  )
+}
+export default CarForm;
---
+++ src/components/CarSearch.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CarSearch = () => {
+  return (
+    <>
+      <div>
+        Car Search
+      </div>
+    </>
+  )
+}
+
+export default CarSearch;
